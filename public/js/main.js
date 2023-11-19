fec17494285ef969e4c6f946620765c33de7bf73
DreCampbell <dillon-campbell@hotmail.com>
2023-11-20 00:17:01 +0800

    Added client side JS

--- public/js/main.js
+++ public/js/main.js
@@ -6,3 +6,19 @@
 // function getHero(){
 //     console.log('Hello World!')
 // }
+
+
+document.querySelector('button').addEventListener('click', apiRequest)
+
+async function apiRequest(){
+    const heroName = document.querySelector('input').value
+    try{
+        const response = await fetch(`https://wandering-cap-fawn.cyclic.app/api/${heroName}`)
+        const data = await response.json()
+
+        console.log(data)
+        // document.querySelector('h2').innerText = data.birthName
+    }catch(error){
+        console.log(error)
+    }
+}
